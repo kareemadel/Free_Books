ee33f93d0397a7f2dba50fe1d49919bee8093681
Kareem Adel <kareem.adel.000@gmail.com>
2018-02-09 08:34:25 +0200

    creating superuser

--- Library/FreeBooks/admin.py
+++ Library/FreeBooks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Author, Book, Category, Profile, Rate, Read, WishList
 
 # Register your models here.
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(Profile)
+admin.site.register(Rate)
+admin.site.register(Read)
+admin.site.register(WishList)
