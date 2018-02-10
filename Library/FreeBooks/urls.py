9fa9ae6ff0d6350a90e252205c832b9589132855
shaimaa_nabil <shaimaahussin44@yahoo.com>
2018-02-10 15:25:44 +0200

    author detail and authors

--- Library/FreeBooks/urls.py
+++ Library/FreeBooks/urls.py
@@ -1,7 +1,10 @@
 
 from django.urls import path
-from .views import home_view
+from .views import home_view,authorsListView,authorsDetailView
 
 urlpatterns = [
-    path('', home_view.as_view(), name='home')
+    path('', home_view.as_view(), name='home'),
+	path('authors/', authorsListView.as_view(), name='AuthorList'),
+	path('authors/<int:pk>', authorsDetailView.as_view(), name='AuthorDetail'),
+
 ]
--- Library/FreeBooks/views.py
+++ Library/FreeBooks/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import Author
+from django.views import generic
 
-app_name = 'FreeBooks'
 
 # Create your views here.
 class home_view(TemplateView):
     def get(self, request):
-        return render(request, 'FreeBooks/base.html')
+        return render(request, 'FreeBooks/HomePage.html')
+
+
+class authorsListView(generic.ListView):
+	model=Author
+
+class authorsDetailView(generic.DetailView):
+    model = Author
