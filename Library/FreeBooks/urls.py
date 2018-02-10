36e2940d65041dae0edf26b169ba02be33ab9483
Kareem Adel <kareem.adel.000@gmail.com>
2018-02-10 15:59:29 +0200

    bookList

--- Library/FreeBooks/urls.py
+++ Library/FreeBooks/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from .views import home_view, book_view
+from .views import home_view, book_view, book_list_view
 
 app_name = 'FreeBooks'
 
 urlpatterns = [
     path('', home_view.as_view(), name='home'),
-    path('books/<pk>', book_view.as_view(), name='book_detail')
+    path('books/<pk>', book_view.as_view(), name='book_detail'),
+    path('books/', book_list_view.as_view(), name='book_list')
 ]
