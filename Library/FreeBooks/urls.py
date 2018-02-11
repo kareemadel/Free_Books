e92ef766dd73db92107b535b7b4b816c8e92c6ac
shaimaa_nabil <shaimaahussin44@yahoo.com>
2018-02-11 14:42:46 +0200

    update css for list and follow button

--- Library/FreeBooks/urls.py
+++ Library/FreeBooks/urls.py
@@ -5,7 +5,7 @@ app_name = 'FreeBooks'
 
 urlpatterns = [
     path('', home_view.as_view(), name='home'),
-    path('books/<pk>', book_view.as_view(), name='book_detail'),
+    path('book/<pk>', book_view.as_view(), name='book_detail'),
     path('books/', book_list_view.as_view(), name='book_list'),
     path('categories/', category_list_view.as_view(), name='category_list'),
     path('categories/<pk>', category_view.as_view(), name='category_detail'),
